cfa9b6a2a7e53b0c4ed62b5430de693d021b966d
shengfeng.wang <shengfeng.wang@dianping.com>
2015-09-22 15:39:56 +0800

    add files

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gitbook build
-cd ..
-cp -rf modify_me/_book/* .
-git commit -am 'add files'
-git push origin master
--- modify_me/deploy.sh
+++ modify_me/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+rm -rf _book
 gitbook build
+rm -rf _book/public _book/deploy.sh
 cd ..
 cp -rf modify_me/_book/* .
 git commit -am 'add files'
